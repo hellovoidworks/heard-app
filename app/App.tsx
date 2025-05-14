50c6f7bd60c95bc5e182690a809c89665d5fe5a2
KP from Voidworks <fungkwokpan@voidworks.co>
2025-05-14 23:47:55 +0800

    Remove wait for ATT

--- app/App.tsx
+++ app/App.tsx
@@ -147,7 +147,6 @@ export default function App() {
       // Initialize the SDK
       Adjust.initSdk(adjustConfig);
       console.log('Adjust SDK initialized successfully');
-      adjustConfig.setAttConsentWaitingInterval(180);
       // Track session_start event when app is first opened
       const startEvent = new AdjustEvent('xe30he');
       Adjust.trackEvent(startEvent);
